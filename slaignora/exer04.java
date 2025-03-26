dfefa0c097fe6215a5523c62921c064dfa982a6f
Raquel Yamauti <yraquellissa@gmail.com>
2025-03-26 13:08:55 -0300

    add o exercicio 3 e 4

---
+++ slaignora/exer04.java
@@ -0,0 +1,23 @@
+//Verificador de Idade: Crie um verificador de idade para identificar se uma pessoa pode votar.
+import java.util.Scanner;
+public class exer04{
+    public static void main (String args[]){
+        Scanner leia = new Scanner(System.in);
+        int idade = 0;
+
+        System.out.print("Por favor, informe a sua idade: ");
+        idade = leia.nextInt();
+
+        if (idade >= 18){
+            System.out.println("Você pode votar");
+        }
+        else{
+            System.out.println("Você não pode votar");
+        }
+
+    }
+}
+
+//O resultado sera assim:
+/*Por favor, informe a sua idade: 18
+Você pode votar*/
